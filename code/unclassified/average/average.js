e77c518041a165cb79ba536b39f75c1d38418e29
Raja sekhar <i.rajasekhar.cse@gmail.com>
2017-10-12 15:40:30 -0400

    Check Edge Cases
    Check for the edge cases to test for proper input

--- code/unclassified/average/average.js
+++ code/unclassified/average/average.js
@@ -3,6 +3,8 @@
 // finds the average of an array of numbers.
 function getAverage(numbers){
     var sum = 0;
+    if(!numbers || !(numbers instanceof Array) ||  numbers.length == 0)
+       return NaN
     for(i=0;i < numbers.length; i++){
         sum += numbers[i];
     }
